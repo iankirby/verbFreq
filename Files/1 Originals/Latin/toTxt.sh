778db7919a5e6a68177fab0452f4b41f4f5752ca
iankirby <55120975+iankirby@users.noreply.github.com>
2022-04-23 13:43:34 -0400

    files moved update

--- Files/1 Originals/Latin/toTxt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for j in *.xml
-do
-cp "$j" "${j%.xml}.txt"
-done
--- scripts/main.py
+++ scripts/main.py
@@ -1,28 +1,39 @@
 import re, subprocess, os, sys
 
+def openDir(name,path):
+    dir=os.listdir(path)
+    
+    if(name=="Greek" or name=="Latin"):
+        i=0
+        while(i<len(dir)):
+            subprocess.call(['python','perseusRegex.py',name,path,dir[i]])
+            i=i+1
 
-
+    else:
+        print("Still need to write English regexes")
 
 def decidePath(var):
     if (var=="Greek" or var=="greek"):
-        title="Greek"
-        path="../Files/Greek/"
-        # print(title)
+        openDir("Greek","../pre/Greek/")
     elif (var=="Latin" or var=="latin"):
-        title="Latin"
-        path="../Files/Latin/"
+        openDir("Latin","../pre/Latin/")
         # print(title)
     elif(var=="Old English"):
-
-
+        openDir(var,"../pre/OldEnglish")
+    elif(var=="Middle English"):
+        openDir(var,"../pre/MiddleEnglish")
+    elif(var=="Early Modern English"):
+        openDir(var,"../pre/EarlyModernEnglish")
+    else:
+        print("don't know")
 
 if(len(sys.argv)<2):
     print("Forgot command line argument")
 else:
     if(sys.argv[1]=="Old"):
-        var="Old English"
-        decidePath()
-    elif():
-    var=sys.argv[1]
-    decidePath(var)
+        decidePath("Old English")
+    elif(sys.argv[1]=="Middle"):
+        decidePath("Middle English")
+    else:
+        decidePath(sys.argv[1])
         
